715377cbdaa6510f3249a9655d1f5a92a9b2832a
abdumalik <mister.ak98@list.ru>
2024-05-29 07:00:25 +0700

    add auth

--- MonitoringDumpTruck/Controllers/DumpTrucksController.cs
+++ MonitoringDumpTruck/Controllers/DumpTrucksController.cs
@@ -8,94 +8,93 @@ using Microsoft.EntityFrameworkCore;
 using MonitoringDumpTruck.Models;
 using MonitoringDumpTruck.Models.Entities;
 
-namespace MonitoringDumpTruck.Controllers
+namespace MonitoringDumpTruck.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DumpTrucksController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DumpTrucksController : ControllerBase
+    private readonly MonitoringContext _context;
+
+    public DumpTrucksController(MonitoringContext context)
     {
-        private readonly MonitoringContext _context;
+        _context = context;
+    }
 
-        public DumpTrucksController(MonitoringContext context)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DumpTruck>>> GetDumpTrucks()
+    {
+        return await _context.DumpTrucks.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<DumpTruck>> GetDumpTruck(int id)
+    {
+        var dumpTruck = await _context.DumpTrucks.FindAsync(id);
+
+        if (dumpTruck == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<DumpTruck>>> GetDumpTrucks()
+        return dumpTruck;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutDumpTruck(int id, DumpTruck dumpTruck)
+    {
+        if (id != dumpTruck.Id)
         {
-            return await _context.DumpTrucks.ToListAsync();
+            return BadRequest();
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<DumpTruck>> GetDumpTruck(int id)
-        {
-            var dumpTruck = await _context.DumpTrucks.FindAsync(id);
+        _context.Entry(dumpTruck).State = EntityState.Modified;
 
-            if (dumpTruck == null)
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!DumpTruckExists(id))
             {
                 return NotFound();
             }
-
-            return dumpTruck;
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutDumpTruck(int id, DumpTruck dumpTruck)
-        {
-            if (id != dumpTruck.Id)
+            else
             {
-                return BadRequest();
+                throw;
             }
-
-            _context.Entry(dumpTruck).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DumpTruckExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        [HttpPost]
-        public async Task<ActionResult<DumpTruck>> PostDumpTruck(DumpTruck dumpTruck)
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DumpTruck>> PostDumpTruck(DumpTruck dumpTruck)
+    {
+        _context.DumpTrucks.Add(dumpTruck);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetDumpTruck", new { id = dumpTruck.Id }, dumpTruck);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDumpTruck(int id)
+    {
+        var dumpTruck = await _context.DumpTrucks.FindAsync(id);
+        if (dumpTruck == null)
         {
-            _context.DumpTrucks.Add(dumpTruck);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetDumpTruck", new { id = dumpTruck.Id }, dumpTruck);
+            return NotFound();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteDumpTruck(int id)
-        {
-            var dumpTruck = await _context.DumpTrucks.FindAsync(id);
-            if (dumpTruck == null)
-            {
-                return NotFound();
-            }
+        _context.DumpTrucks.Remove(dumpTruck);
+        await _context.SaveChangesAsync();
 
-            _context.DumpTrucks.Remove(dumpTruck);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-
-        private bool DumpTruckExists(int id)
-        {
-            return _context.DumpTrucks.Any(e => e.Id == id);
-        }
+    private bool DumpTruckExists(int id)
+    {
+        return _context.DumpTrucks.Any(e => e.Id == id);
     }
 }
---
+++ MonitoringDumpTruck/Controllers/UsersController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore;
+using MonitoringDumpTruck.Helpers;
+using MonitoringDumpTruck.Models;
+using MonitoringDumpTruck.Models.Entities;
+using System.Security.Claims;
+using System.Text;
+
+namespace MonitoringDumpTruck.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly MonitoringContext _context;
+
+    public UsersController(MonitoringContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] User model)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        string passwordHash
+                = BCrypt.Net.BCrypt.HashPassword(model.Password);
+
+        var userExists = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+        if (userExists != null)
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new Response { Status = "Error", Message = "User already exists!" });
+
+
+        User user = new User()
+        {
+            Email = model.Email,
+            Name = model.Name,
+            Password = passwordHash,
+            RoleId = 1
+        };
+
+        try
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new Response { Status = "Success", Message = "User created successfully!" });
+        }
+        catch (Exception)
+        {
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new Response { Status = "Error", 
+                    Message = "User creation failed! Please check user details and try again." });
+        }
+
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] User model)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+
+        if (user == null)
+            return Unauthorized();
+       
+        var userPassword = BCrypt.Net.BCrypt.Verify(model.Password, user.Password);
+
+        if (user == null && userPassword)
+            return Unauthorized("Email not found and/or password incorrect");
+
+        if (user != null && userPassword)
+        {
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            return Ok(new Response { Status = "Success", Message = "User sign in successfully!" });
+        }
+        return Unauthorized();
+    }
+
+}
